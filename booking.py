1fb9219ae9ef01d4f1dc852f12d04e846e4a78a9
dodo <dodo@chaosdorf.de>
2026-02-03 22:42:10 +0100

    Implements overtime calculation.

--- booking.py
+++ booking.py
@@ -1,13 +1,17 @@
+import pendulum
 from pendulum import Date, Duration
 
 from workday import WorkDay
 
 
 class Booking:
+    OVERTIME_THRESHOLD: Duration = pendulum.duration(hours=8)
+
     date: Date
     total: Duration
     work: Duration
     breaks: Duration
+    balance: Duration
 
     def __init__(self, date: Date, total: Duration, work: Duration, breaks: Duration):
         self.date = date
@@ -15,6 +19,15 @@ class Booking:
         self.total = total
         self.work = work
         self.breaks = breaks
+        self.balance = work - self.OVERTIME_THRESHOLD
+
+    def has_overtime(self) -> bool:
+        """Returns whether this booking has overtime."""
+        return self.work > self.OVERTIME_THRESHOLD
+
+    def has_undertime(self) -> bool:
+        """Returns whether this booking is below time requirement."""
+        return self.work < self.OVERTIME_THRESHOLD
 
     @staticmethod
     def from_workday(wd: WorkDay) -> Booking:
