0636d4ef94b14f8f29d0cb997a30034e8dc9be84
dodo <dodo@chaosdorf.de>
2026-02-03 20:41:35 +0100

    Implements WorkDay class.

--- workday.py
+++ workday.py
@@ -1,169 +1,101 @@
-import json
 from typing import Optional
 
-from pendulum import DateTime, Duration, Date
+from pendulum import Date
+
+from entry import Entry
 
 
 class WorkDay:
+    """A WorkDay contains the time data for the work time and the breaks during one workday."""
+
     date: Date
-    work_start: DateTime | None
-    work_end: DateTime | None
-    break_start: DateTime | None
-    break_end: DateTime | None
+    entries: list[Entry]
+    state: str
 
     def __init__(
         self,
         date: Optional[Date] = None,
-        work_start: Optional[DateTime] = None,
-        work_end: Optional[DateTime] = None,
-        break_start: Optional[DateTime] = None,
-        break_end: Optional[DateTime] = None,
     ):
         self.date = date
-        self.work_start = work_start
-        self.work_end = work_end
-        self.break_start = break_start
-        self.break_end = break_end
+        self.entries = []
+        self.state = "before_work"
+        # Possible states: before_work, during_work, during_break, after_work
 
-    def total_duration(self) -> Duration | None:
-        """Returns the total time, disregarding the break.
-
-        Returns None if times are undefined.
-        """
-        if self.work_start is None or self.work_end is None:
-            return None
-        return self.work_end - self.work_start
-
-    def break_duration(self) -> Duration | None:
-        """Returns the break time of the workday.
-
-        Returns None if times are undefined.
-        """
-        if self.break_start is None or self.break_end is None:
-            return None
-
-        return self.break_end - self.break_start
-
-    def work_duration(self) -> Duration | None:
-        """Returns the actual work time.
-
-        Deducts the break from the total time.
-        Returns None if times are undefined.
-        """
-        total_time = self.total_duration()
-        if total_time is None:
-            return None
-
-        break_time: Duration | None = self.break_duration()
-        if break_time is None:
-            return None
-
-        return total_time - break_time
-
-    def is_finished(self) -> bool:
-        """Returns True if the workday contains all mandatory times.
-
-        Work start time and work end time are always required.
-        Break start time is required, if a break end time is provided.
-        Break end time is required, if a break start time is provided.
-        Comments are always optional.
-        """
-        if self.work_start is None or self.work_end is None:
-            return False
-
-        if self.break_start is None and self.break_end is None:
-            return True
-
-        return False
-
-    def set(self, field: int, value: DateTime) -> None:
-        """Sets workday field to given value.
-
-        Dependencies for field must be fulfilled."""
-        match field:
-            case 0:
-                self._set_work_start(value)
-            case 1:
-                self._set_work_end(value)
-            case 2:
-                self._set_break_start(value)
-            case 3:
-                self._set_break_end(value)
+    def add_entry(self, entry: Entry):
+        match entry.entry_type:
+            case "work_start":
+                self._set_work_start(entry)
+            case "work_end":
+                self._set_work_end(entry)
+            case "break_start":
+                self._set_break_start(entry)
+            case "break_end":
+                self._set_break_end(entry)
             case _:
                 return
 
-    def _set_work_start(self, dt: DateTime) -> None:
-        if self.work_end is not None:
-            assert dt < self.work_end, "work start must happen before the work end"
-        self.work_start = dt
-        self.date = dt.date()
+        # Sort the entries chronological
+        self.entries.sort(key=lambda entry: entry.dt)
 
-    def _set_work_end(self, dt: DateTime) -> None:
-        assert self.work_start is not None, "don't set end of work, if start is missing"
-        assert dt > self.work_start, "work end must happen after the work start"
-        self.work_end = dt
+    def _set_work_start(self, entry: Entry):
+        if self.state != "before_work":
+            return
+        self.entries.insert(0, entry)
+        self.state = "during_work"
 
-    def _set_break_start(self, dt: DateTime) -> None:
-        assert self.work_start is not None, (
-            "before any break there has to be a work start"
-        )
-        assert dt > self.work_start, "break must happen after the work start"
-        if self.work_end is not None:
-            assert dt < self.work_end, "break must be started before the work end"
+    def _set_work_end(self, entry: Entry):
+        if self.state != "during_work":
+            return
+        self.entries.insert(-1, entry)
+        self.state = "after_work"
 
-        self.break_start = dt
+    def _set_break_start(self, entry: Entry):
+        if self.state != "during_work":
+            return
+        self.entries.append(entry)
+        self.state = "during_break"
 
-    def _set_break_end(self, dt: DateTime) -> None:
-        assert self.work_start is not None and self.break_start is not None, (
-            "don't set end of break, without start of work / break"
-        )
-        assert dt > self.break_start, "end of break must happen after start of break"
+    def _set_break_end(self, entry: Entry):
+        if self.state != "during_break":
+            return
+        self.entries.append(entry)
+        self.state = "during_work"
 
-        if self.work_end is not None:
-            assert dt < self.work_end, "break must be finished before work end"
+    def is_valid(self):
+        """Returns if the workday is valid.
 
-        self.break_end = dt
+        Each workday has to have:
+        - exactly one entry defining the work start
+        - exactly one entry defining the work end
 
-    def dumps(self) -> str:
-        """Returns JSON data of the work day as a string."""
-        json_data = {
-            "date": self.date.isoformat() if self.date is not None else None,
-            "work_start": self.work_start.isoformat()
-            if self.work_start is not None
-            else None,
-            "work_end": self.work_end.isoformat()
-            if self.work_end is not None
-            else None,
-            "break_start": self.break_start.isoformat()
-            if self.break_start is not None
-            else None,
-            "break_end": self.break_end.isoformat()
-            if self.break_end is not None
-            else None,
-        }
-        return json.dumps(json_data, indent=4)
+        Additionally, each workday can have entries defining the breaks.
+        For each break start, there has to be a corresponding entry defining the break end.
+        """
+        if self.entries[0].entry_type != "work_start":
+            # return False if work start is missing
+            return False
+        if self.entries[-1].entry_type != "work_end":
+            # return False if work end is missing
+            return False
 
-    @staticmethod
-    def loads(json_str: str) -> WorkDay:
-        """Loads WorkDay from JSON data."""
-        json_data = json.loads(json_str)
-        day = json_data["date"]
-        work_start = json_data["work_start"]
-        work_end = json_data["work_end"]
-        break_start = json_data["break_start"]
-        break_end = json_data["break_end"]
+        on_break = False
+        for entry in self.entries[1:-1]:
+            if entry.entry_type == "break_start" and on_break:
+                # return False if a break start is in the wrong place
+                return False
 
-        wd = WorkDay(
-            date=DateTime.fromisoformat(day).date() if day is not None else None,
-            work_start=DateTime.fromisoformat(work_start)
-            if work_start is not None
-            else None,
-            work_end=DateTime.fromisoformat(work_end) if work_end is not None else None,
-            break_start=DateTime.fromisoformat(break_start)
-            if break_start is not None
-            else None,
-            break_end=DateTime.fromisoformat(break_end)
-            if break_end is not None
-            else None,
-        )
-        return wd
+            if entry.entry_type == "break_end" and not on_break:
+                # return False if a break end is in the wrong place
+                return False
+
+            if entry.entry_type == "break_start":
+                on_break = True
+
+            if entry.entry_type == "break_end":
+                on_break = False
+
+        # return False if the state is not "after_work"
+        if self.state != "after_work":
+            return False
+
+        return True
