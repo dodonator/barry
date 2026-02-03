a4f672fff421c037b8b89bdd9046befd72029422
dodo <dodo@chaosdorf.de>
2026-02-03 20:00:25 +0100

    Adds methods for conversion to dict.

--- entry.py
+++ entry.py
@@ -1,5 +1,13 @@
 import uuid
+
 import pendulum
+
+ENTRY_TYPES: dict[int, str] = {
+    0: "work_start",
+    1: "work_end",
+    2: "break_start",
+    3: "break_end",
+}
 
 
 class Entry:
@@ -15,3 +23,23 @@ class Entry:
         self.entry_type = entry_type
         self.dt = dt
         self.comment = comment
+
+    def to_dict(self) -> dict[str, str]:
+        """Returns entry as a dict."""
+        return {
+            "entry_id": str(self.entry_id),
+            "entry_type": self.entry_type,
+            "dt": self.dt.isoformat(),
+            "comment": self.comment,
+        }
+
+    @staticmethod
+    def from_dict(data: dict[str, str]) -> Entry:
+        """Creates entry from a dict."""
+        entry = Entry(
+            entry_type=data["entry_type"],
+            dt=pendulum.DateTime.fromisoformat(data["dt"]),
+            comment=data["comment"],
+        )
+        entry.entry_id = uuid.UUID(data["entry_id"])
+        return entry
